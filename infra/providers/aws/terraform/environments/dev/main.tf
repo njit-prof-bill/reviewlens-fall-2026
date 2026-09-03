70560d08ea702841e76a367f9f6c7b86ff0a7a29
Bill McCann <bill.mccann@gmail.com>
2026-09-03 17:35:15 -0400

    Update terraform for AWS deploy

--- infra/providers/aws/terraform/environments/dev/main.tf
+++ infra/providers/aws/terraform/environments/dev/main.tf
@@ -97,8 +97,8 @@ module "ecs" {
   database_url_arn         = var.rds_database_url_arn
   clerk_jwks_url_param_arn = module.secrets.ssm_clerk_jwks_url_arn
   cors_origins_param_arn   = module.secrets.ssm_cors_origins_arn
-  clerk_issuer_param_arn   = module.secrets.ssm_clerk_issuer_arn
-  clerk_audience_param_arn = module.secrets.ssm_clerk_audience_arn
+  clerk_issuer_param_arn   = var.clerk_issuer != "" ? module.secrets.ssm_clerk_issuer_arn : ""
+  clerk_audience_param_arn = var.clerk_audience != "" ? module.secrets.ssm_clerk_audience_arn : ""
 }
 
 # Frontend (S3 + CloudFront)
