81a58d9f6879d339775d4b3717b568ed343deed0
Bill McCann <bill.mccann@gmail.com>
2026-09-03 15:53:51 -0400

    Fix deploy issues

--- apps/api/alembic/env.py
+++ apps/api/alembic/env.py
@@ -8,7 +8,7 @@ from app.db import models  # noqa: F401
 from app.db.session import Base
 
 config = context.config
-config.set_main_option("sqlalchemy.url", settings.database_url)
+config.set_main_option("sqlalchemy.url", settings.database_url.replace("%", "%%"))
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
